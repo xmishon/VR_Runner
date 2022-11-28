2743853fe7c67acb011d4e787ef8be624f269407
Mikhail Zmeevskiy <xmishon17@gmail.com>
2022-11-29 01:59:47 +0400

    Added obstacle spawner and obstacle killer

--- Assets/Code/ObstacleKiller.cs
+++ Assets/Code/ObstacleKiller.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ObstacleKiller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _player;
+    [SerializeField] private ObstacleSpawner _spawner;
+    [SerializeField] private float _killDistanceZ;
 
     // Update is called once per frame
     void Update()
     {
-        
+        List<Transform> obstacles = _spawner.spawnedObstacles;
+
+        for(int i = 0; i < obstacles.Count; i++)
+        {
+            if (_player.position.z > obstacles[i].position.z + _killDistanceZ)
+            {
+                Destroy(obstacles[i].gameObject);
+            }
+        }
     }
 }
--- Assets/Code/ObstacleSpawner.cs
+++ Assets/Code/ObstacleSpawner.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    [SerializeField] private Transform[] _obstacles;
+    [SerializeField] private float _spawnStep;
+    [SerializeField] private float _spawnDistance;
+    [SerializeField] private Vector2 _segmentWidth;
+
+    private Transform _myTrans;
+    private Vector3 _lastPos;
+
+    private List<Transform> _spawnedObstacles = new List<Transform>();
+    public List<Transform> spawnedObstacles { get { _spawnedObstacles.RemoveAll(TransformIsNull); return _spawnedObstacles; } }
+
+    private bool TransformIsNull(Transform a)
+    {
+        return a == null;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myTrans = transform;
+        _lastPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_myTrans.position.z > _lastPos.z + _spawnStep)
+        {
+            _lastPos.z += _spawnStep;
+
+            Transform newObstacle = _obstacles[Random.Range(0, _obstacles.Length)];
+            _spawnedObstacles.Add(Instantiate(newObstacle,
+                new Vector3(Random.Range(_segmentWidth.x, _segmentWidth.y),
+                0,
+                _lastPos.z + _spawnDistance
+                ), Quaternion.identity));
+        }
     }
 }
